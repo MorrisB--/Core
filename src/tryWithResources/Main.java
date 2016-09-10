f33a1157fe7250842047194545ae9915bcb828eb
MorrisB-- <morris.ballenger@yahoo.com>
2016-09-10 16:43:17 -0500

    Added doTryWithResourcesMulti()

--- src/tryWithResources/Main.java
+++ src/tryWithResources/Main.java
@@ -2,24 +2,27 @@ package tryWithResources;
 
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 
 public class Main {
-	
-	public static void main(String[] args){
-		
-		
+
+	public static void main(String[] args) {
+
+		// doTryCatchFinally();
+		// doTryWithResources();
+		// doTryWithResourcesMulti();
 	}
-	
+
 	public static void doTryCatchFinally() {
 		char[] buff = new char[8];
 		int length;
 		Reader reader = null;
-		
+
 		try {
 			reader = Helper.openReader("file1.txt");
-			while ((length = reader.read(buff))>=0) {
-				System.out.println("/nlength: " + length);
-				for(int i = 0;i<length;i++)
+			while ((length = reader.read(buff)) >= 0) {
+				System.out.println("\nlength: " + length);
+				for (int i = 0; i < length; i++)
 					System.out.print(buff[i]);
 			}
 		} catch (IOException e) {
@@ -33,21 +36,34 @@ public class Main {
 			}
 		}
 	}
-	
+
 	public static void doTryWithResources() {
 		char[] buff = new char[8];
 		int length;
-		
-		try (Reader reader = Helper.openReader("file1.txt");
-) {
-			while ((length = reader.read(buff))>=0) {
-				System.out.println("/nlength: " + length);
-				for(int i = 0;i<length;i++)
+
+		try (Reader reader = Helper.openReader("file1.txt");) {
+			while ((length = reader.read(buff)) >= 0) {
+				System.out.println("\nlength: " + length);
+				for (int i = 0; i < length; i++)
 					System.out.print(buff[i]);
 			}
 		} catch (IOException e) {
 			System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
-		} 
+		}
+	}
+
+	public static void doTryWithResourcesMulti() {
+		char[] buff = new char[8];
+		int length;
+
+		try (Reader reader = Helper.openReader("file1.txt"); Writer writer = Helper.openWriter("file2.txt");) {
+			while ((length = reader.read(buff)) >= 0) {
+				System.out.println("\nlength: " + length);
+				writer.write(buff, 0, length);
+			}
+		} catch (IOException e) {
+			System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+		}
 	}
 
 }
