ae4606134fd4975b954cb24a5d1a2d509c5c3059
MorrisB-- <morris.ballenger@yahoo.com>
2016-09-10 16:36:53 -0500

    Added doTryWithResoureces

--- src/tryWithResources/Main.java
+++ src/tryWithResources/Main.java
@@ -7,16 +7,15 @@ public class Main {
 	
 	public static void main(String[] args){
 		
+		
 	}
 	
 	public static void doTryCatchFinally() {
-		
 		char[] buff = new char[8];
 		int length;
 		Reader reader = null;
 		
 		try {
-			
 			reader = Helper.openReader("file1.txt");
 			while ((length = reader.read(buff))>=0) {
 				System.out.println("/nlength: " + length);
@@ -33,6 +32,22 @@ public class Main {
 				System.out.println(e2.getClass().getSimpleName() + " - " + e2.getMessage());
 			}
 		}
+	}
+	
+	public static void doTryWithResources() {
+		char[] buff = new char[8];
+		int length;
+		
+		try (Reader reader = Helper.openReader("file1.txt");
+) {
+			while ((length = reader.read(buff))>=0) {
+				System.out.println("/nlength: " + length);
+				for(int i = 0;i<length;i++)
+					System.out.print(buff[i]);
+			}
+		} catch (IOException e) {
+			System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+		} 
 	}
 
 }
