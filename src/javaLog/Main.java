ffb4b286540861f58693ffe88f1960f26d1c8da3
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-16 10:01:57 -0500

    Useed logger within class

--- src/javaLog/Main.java
+++ src/javaLog/Main.java
@@ -1,5 +1,16 @@
 package javaLog;
 
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class Main {
+	public static void main(String[] args) {
+		
+		LogManager logManager = LogManager.getLogManager();
+		Logger logger = logManager.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		logger.log(Level.INFO, "Testing log messages");
+		logger.log(Level.INFO, "Another message");
+	}
 
 }
