f56abe26f395edccfc97130b016b511807a63f0d
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 15:30:59 -0500

    Main will now wait for thread to finish.

--- src/multithreading/Main.java
+++ src/multithreading/Main.java
@@ -4,11 +4,21 @@ public class Main {
 	public static void main(String[] args) {
 		String[] inFiles = { "./file1.txt", "./file2.txt" };
 		String[] outFiles = { "./file1.out.txt", "./file2.out.txt" };
+		Thread[] threads = new Thread[inFiles.length];
 
 		for (int i = 0; i < inFiles.length; i++) {
 			Adder adder = new Adder(inFiles[i], outFiles[i]);
-			Thread thread = new Thread(adder);
-			thread.start();
+			threads[i] = new Thread(adder);
+			threads[i].start();
+		}
+
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
