6d46e316c0bbb7baf17663f2dcba4d71343cac4a
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 15:10:16 -0500

    Finished main(String)

--- src/multithreading/Main.java
+++ src/multithreading/Main.java
@@ -1,8 +1,19 @@
 package multithreading;
 
+import java.io.IOException;
+
 public class Main {
+	public static void main(String[] args) {
+		String[] inFiles = { "./file1.txt", "./file2.txt" };
+		String[] outFiles = { "./file1.out.txt", "./file2.out.txt" };
 
-	String[] inFiles = {"./file1.txt", "./file2.txt"};
-	String[] outFiles = {"./file1.out.txt", "./file2.out.txt"};
+		try {
+			for (int i = 0; i < inFiles.length; i++) {
+				Adder adder = new Adder(inFiles[i], outFiles[i]);
+				adder.doAdd();
+			}
+		} catch (IOException e) {
 
+		}
+	}
 }
