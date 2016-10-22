f0bc9e70bad031705ebb3ed37d483e0e5f1ac18e
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 14:53:14 -0500

    Finished doAdd()

--- src/multithreading/Adder.java
+++ src/multithreading/Adder.java
@@ -1,5 +1,11 @@
 package multithreading;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Adder {
 
 	private String inFile, outFile;
@@ -8,8 +14,19 @@ public class Adder {
 
 	}
 
-	public void doAdd() {
+	public void doAdd() throws IOException {
 
+		int total = 0;
+		String line = null;
+		
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(inFile))){
+			while ((line = reader.readLine()) !=null)
+				total += Integer.parseInt(line);
+		}
+		
+		try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(outFile))){
+			writer.write("Total: " + total);
+		}
 	}
 
 }
