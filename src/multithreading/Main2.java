ccae391296361e7b8f79ad9c65ed4b6062bc2513
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 21:39:54 -0500

    Finished mroe methods

--- src/multithreading/Main2.java
+++ src/multithreading/Main2.java
@@ -10,23 +10,23 @@ public class Main2 {
 		/*
 		 * Using Worker.java
 		 */
-		
+
 		ExecutorService es = Executors.newFixedThreadPool(5);
 		BankAccount account = new BankAccount(100);
-		
+
 		for (int i = 0; i < 10; i++) {
 			Worker worker = new Worker(account);
 			es.submit(worker);
 		}
-		
+
 		/*
 		 * Using TransactionWorker.java
 		 */
-		
+
 		ExecutorService es1 = Executors.newFixedThreadPool(5);
-		TransactionWorker[] workers = null; // set this later, example
-		
-		for(TransactionWorker worker: workers)
+		TransactionWorker[] workers = null; // Retrieve TransactionPromoWorkers
+
+		for (TransactionWorker worker : workers)
 			es1.submit(worker);
 	}
 
---
+++ src/multithreading/TransactionPromoWorker.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+public class TransactionPromoWorker extends TransactionWorker {
+
+	public TransactionPromoWorker(BankAccount account, char transactionType, int amount) {
+		super(account, transactionType, amount);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		if (transactionType == 'w')
+			account.withdrawal(amount);
+		else if (transactionType == 'd') {
+			account.deposit(amount);
+			/*
+			 * Giving a 10% bonus if the account has more than $500 after the
+			 * deposit
+			 */
+			if (account.getBalance() > 500) {
+				int bonus = (int) ((account.getBalance() - 500) * 0.1);
+				account.deposit(bonus);
+			}
+		}
+	}
+
+}
