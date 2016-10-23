191f061efcda6bd62340261daf957e8323c3b317
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 20:56:51 -0500

    Created BankAccount(int), getBalance(), and deposit(int)

--- src/multithreading/BankAccount.java
+++ src/multithreading/BankAccount.java
@@ -2,4 +2,17 @@ package multithreading;
 
 public class BankAccount {
 
+	private int balance;
+
+	public BankAccount(int startingBalance) {
+		this.balance = startingBalance;
+	}
+
+	public int getBalance() {
+		return this.balance;
+	}
+
+	public void deposit(int amount) {
+		balance += amount;
+	}
 }
