3ec25fb85c3fa5d534b9222de4e2b3994c413b03
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 21:11:15 -0500

    Created concurrency issues

--- src/multithreading/Main2.java
+++ src/multithreading/Main2.java
@@ -1,5 +1,19 @@
 package multithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main2 {
+	public static void main(String[] args) {
+
+		ExecutorService es = Executors.newFixedThreadPool(5);
+		BankAccount account = new BankAccount(100);
+		
+		for (int i = 0; i < 10; i++) {
+			Worker worker = new Worker(account);
+			es.submit(worker);
+		}
+		
+	}
 
 }
