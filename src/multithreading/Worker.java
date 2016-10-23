22873e1e524b52e1d21de858b35c1fe9da007562
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 21:05:59 -0500

    Worker now implements Runnable

--- src/multithreading/Worker.java
+++ src/multithreading/Worker.java
@@ -1,5 +1,20 @@
 package multithreading;
 
-public class Worker {
+public class Worker implements Runnable {
 
+	private BankAccount account;
+
+	public Worker(BankAccount account) {
+		this.account = account;
+	}
+
+	public void run() {
+
+		for (int i = 0; i < 10; i++) {
+			int startBalance = account.getBalance();
+			account.deposit(10);
+			int endBalance = account.getBalance();
+		}
+		
+	}
 }
