318fd87ac0896766010d4a24bdda8ba37394b340
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 21:20:53 -0500

    Created withdrawal(int) method

--- src/multithreading/BankAccount.java
+++ src/multithreading/BankAccount.java
@@ -12,7 +12,11 @@ public class BankAccount {
 		return this.balance;
 	}
 
-	public void deposit(int amount) {
-		balance += amount;
+	public synchronized void deposit(int amount) {
+		this.balance += amount;
+	}
+
+	public synchronized void withdrawal(int amount) {
+		this.balance -= amount;
 	}
 }
