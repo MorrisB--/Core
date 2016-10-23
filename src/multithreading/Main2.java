183e72ef98e18864fafcf5ceceb13f871ccde445
MorrisB-- <morris.ballenger@yahoo.com>
2016-10-22 21:32:10 -0500

    Used TransactionWorker

--- src/multithreading/Main2.java
+++ src/multithreading/Main2.java
@@ -4,8 +4,13 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Main2 {
+	@SuppressWarnings("null")
 	public static void main(String[] args) {
 
+		/*
+		 * Using Worker.java
+		 */
+		
 		ExecutorService es = Executors.newFixedThreadPool(5);
 		BankAccount account = new BankAccount(100);
 		
@@ -14,6 +19,15 @@ public class Main2 {
 			es.submit(worker);
 		}
 		
+		/*
+		 * Using TransactionWorker.java
+		 */
+		
+		ExecutorService es1 = Executors.newFixedThreadPool(5);
+		TransactionWorker[] workers = null; // set this later, example
+		
+		for(TransactionWorker worker: workers)
+			es1.submit(worker);
 	}
 
 }
--- src/multithreading/TransactionWorker.java
+++ src/multithreading/TransactionWorker.java
@@ -1,5 +1,21 @@
 package multithreading;
 
-public class TransactionWorker {
+public class TransactionWorker implements Runnable {
+
+	protected BankAccount account;
+	protected char transactionType; // w for withdrawal d for deposit
+	protected int amount;
+
+	public TransactionWorker(BankAccount account, char transactionType, int amount) {
+		this.account = account;
+	}
+
+	@Override
+	public void run() {
+		if (transactionType == 'w')
+			account.withdrawal(amount);
+		else if (transactionType == 'd')
+			account.deposit(amount);
+	}
 
 }
